bea61f816518142d2ad8a1a9b2e36a46e227135c
paalamugan <paalamugan@gmail.com>
2023-03-13 04:36:01 +0530

    chore: minor change

--- app/routes/delete.ts
+++ app/routes/delete.ts
@@ -7,9 +7,10 @@ import { OUTPUT_DIR } from "@/constants/common";
 export async function loader({ request }: LoaderArgs) {
   try {
     const videoDir = path.resolve(OUTPUT_DIR);
+    const files = fse.readdirSync(videoDir);
     fse.removeSync(videoDir);
     return json({
-      message: "successfully deleted all videos",
+      message: `Successfully Deleted ${files.length} files.`,
     });
   } catch (error: unknown) {
     console.log("error ❌", { error });
