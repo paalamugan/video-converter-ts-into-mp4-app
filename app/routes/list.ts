ce81bc186bef28d1799bc6fce6f474a89da37da7
paalamugan <paalamugan@gmail.com>
2023-03-13 03:40:28 +0530

    testing 2

--- app/routes/list.ts
+++ app/routes/list.ts
@@ -3,12 +3,15 @@ import type { LoaderArgs } from "@remix-run/server-runtime";
 import { json } from "@remix-run/server-runtime";
 import path from "path";
 import fs from "fs";
+import ffprobePath from "@ffprobe-installer/ffprobe";
 export async function loader({ request }: LoaderArgs) {
+  console.log("🚀 ~ file: list.ts:7 ~ ffprobePath:", ffprobePath);
   try {
     const dir = path.join(process.cwd(), "node_modules");
     const files = fs.readdirSync(dir);
     return json({
       files,
+      ffprobePath,
     });
   } catch (error: unknown) {
     console.log("error ❌", { error });
