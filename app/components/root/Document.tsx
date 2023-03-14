a6299331a1decffe1e0df400d81f022b5cde0f5c
paalamugan <paalamugan@gmail.com>
2023-03-15 05:04:01 +0530

    chore: added favicon

--- app/components/root/Document.tsx
+++ app/components/root/Document.tsx
@@ -74,6 +74,24 @@ export const Document = withEmotionCache(
       >
         <head>
           {title && <title>{title}</title>}
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/site.webmanifest" />
           <Meta />
           <Links />
           {serverStyles?.map(({ key, ids, css }) => (
